cb49f825b557be7fc35153ed73db10d854ca78d0
jpavia10 <jpavia10@gmail.com>
2026-09-25 09:38:25 -0700

    v3 midnight museum redesign + Ultralounge first (js/playlist.js)

--- js/playlist.js
+++ js/playlist.js
@@ -3,13 +3,13 @@
    shown in the player UI). They are not committed to the repo and will not work
    offline (see CHANGELOG.md). Edit this array by hand to swap tracks. */
 window.TITAN_PLAYLIST = [
+  { title: "Ultralounge", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Ultralounge.mp3", license: "CC-BY", credit: "\u201CUltralounge\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Chill Wave", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Chill%20Wave.mp3", license: "CC-BY", credit: "\u201CChill Wave\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Chillin Hard", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Chillin%20Hard.mp3", license: "CC-BY", credit: "\u201CChillin Hard\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Suave Standpipe", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Suave%20Standpipe.mp3", license: "CC-BY", credit: "\u201CSuave Standpipe\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Ice Flow", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Ice%20Flow.mp3", license: "CC-BY", credit: "\u201CIce Flow\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Soporific", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Soporific.mp3", license: "CC-BY", credit: "\u201CSoporific\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Kool Kats", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Kool%20Kats.mp3", license: "CC-BY", credit: "\u201CKool Kats\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
-  { title: "Ultralounge", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Ultralounge.mp3", license: "CC-BY", credit: "\u201CUltralounge\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Mirage", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Mirage.mp3", license: "CC-BY", credit: "\u201CMirage\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Local Forecast - Slower", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Local%20Forecast%20-%20Slower.mp3", license: "CC-BY", credit: "\u201CLocal Forecast - Slower\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
   { title: "Disco Lounge", artist: "Kevin MacLeod", url: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Disco%20Lounge.mp3", license: "CC-BY", credit: "\u201CDisco Lounge\u201D Kevin MacLeod (incompetech.com) \u2014 CC BY 4.0" },
